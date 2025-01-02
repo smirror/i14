709918284064f7c862501e1f54e2a5b137bd0bc0
smirror <23025996+smirror@users.noreply.github.com>
2025-01-02 22:10:34 +0900

    Revert "配車するとき一番近い空いてる椅子を選ぶ"
    This reverts commit 758858437bc1e20ce1a94ab39f2e0a0539d535f9.

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -9,6 +9,7 @@ import (
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
 	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
@@ -22,18 +23,7 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	matched := &Chair{}
 	empty := false
 	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, `
-			SELECT c.* FROM chairs c
-			INNER JOIN (
-				SELECT chair_id, MAX(created_at) as latest
-				FROM chair_locations
-				GROUP BY chair_id
-			) latest_cl ON c.id = latest_cl.chair_id
-			INNER JOIN chair_locations cl ON latest_cl.chair_id = cl.chair_id AND latest_cl.latest = cl.created_at
-			WHERE c.is_active = TRUE
-			ORDER BY ABS(cl.latitude - ?) + ABS(cl.longitude - ?)
-			LIMIT 1
-		`, ride.PickupLatitude, ride.PickupLongitude); err != nil {
+		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
 				w.WriteHeader(http.StatusNoContent)
 				return
