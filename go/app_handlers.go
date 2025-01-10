d33fc17f78719165e5753474de68c1e479e11b45
smirror <23025996+smirror@users.noreply.github.com>
2025-01-10 22:31:48 +0900

    コード修正

--- go/app_handlers.go
+++ go/app_handlers.go
@@ -831,6 +831,20 @@ type appGetNearbyChairsResponseChair struct {
 	CurrentCoordinate Coordinate `json:"current_coordinate"`
 }
 
+type ChairWithLatLon struct {
+	ID          string    `db:"id"`
+	OwnerID     string    `db:"owner_id"`
+	Name        string    `db:"name"`
+	Model       string    `db:"model"`
+	IsActive    bool      `db:"is_active"`
+	AccessToken string    `db:"access_token"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
+
+	Latitude  int `db:"latitude"`
+	Longitude int `db:"longitude"`
+}
+
 func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	latStr := r.URL.Query().Get("latitude")
@@ -869,70 +883,52 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 	}
 	defer tx.Rollback()
 
-	chairsWithDetails := []struct {
-		ChairID    string `db:"chair_id"`
-		ChairName  string `db:"chair_name"`
-		ChairModel string `db:"chair_model"`
-		IsActive   bool   `db:"is_active"`
-		Latitude   int    `db:"latitude"`
-		Longitude  int    `db:"longitude"`
-		Distance   int    `db:"distance"`
-	}{}
-
-	err = tx.SelectContext(
-		ctx,
-		&chairsWithDetails,
+	chairs := []ChairWithLatLon{}
+	err = tx.Select(
+		&chairs,
 		`
-WITH latest_chair_locations AS (
-    SELECT cl.*
-    FROM (
-        SELECT cl.*, ROW_NUMBER() OVER (PARTITION BY cl.chair_id ORDER BY cl.created_at DESC) AS rn
-        FROM chair_locations cl
-        WHERE cl_inner.chair_id = cl.chair_id
-    ) cl
-    WHERE cl.rn = 1
-    AND ABS(cl.latitude - ?) + ABS(cl.longitude - ?) < ?
-),
-latest_ride_statuses AS (
-    SELECT rs.ride_id, rs.status
-    FROM (
-        SELECT rides.*, ride_statuses.status, ROW_NUMBER() OVER (PARTITION BY ride_id ORDER BY ride_statuses.created_at DESC) AS rn
-        FROM rides
-		LEFT JOIN ride_statuses ON rides.id = ride_statuses.ride_id
-    ) r
-	WHERE r.rn = 1
-    AND r.ride_statuses.status = 'COMPLETED'
-)
-SELECT 
-    c.id, 
-    c.name, 
-    c.model, 
-    cl.latitude, 
-    cl.longitude
-FROM 
-    chairs c
-INNER JOIN 
-    latest_chair_locations cl 
-    ON c.id = cl.chair_id
-LEFT JOIN 
-    latest_ride_statuses rs 
-    ON r.id = rs.ride_id
-WHERE 
-    (rs.status = 'COMPLETED' OR rs.status IS NULL)
-    AND c.is_active
-    `, lat, lon, distance,
+ -- 近くに存在している椅子一覧
+ WITH near_chairs AS (
+ 	SELECT cl.*
+ 	FROM (
+ 	    SELECT cl.*, row_number() over (partition BY chair_id ORDER BY created_at DESC) AS rn
+ 	    FROM chair_locations cl
+ 	) cl
+ 	WHERE cl.rn = 1 AND abs(cl.latitude - ?) + abs(cl.longitude - ?) < ?
+ ),
+ -- すべての椅子の最新のステータス
+ chair_latest_status AS (
+ 	SELECT *
+ 	FROM (
+ 		SELECT rides.*, ride_statuses.status AS ride_status, row_number() over (partition BY chair_id ORDER BY ride_statuses.created_at DESC) AS rn
+ 		FROM rides LEFT JOIN ride_statuses ON rides.id = ride_statuses.ride_id		
+ 	) r 
+ 	WHERE r.rn = 1 AND r.ride_status = 'COMPLETED'
+ )
+ 
+ SELECT
+ 	chairs.*, near_chairs.latitude, near_chairs.longitude
+ FROM 
+ 	chairs
+ -- ここのINNER JOINで近くに存在している椅子に絞り込まれる
+ INNER JOIN near_chairs ON chairs.id = near_chairs.chair_id
+ LEFT JOIN chair_latest_status ON chairs.id = chair_latest_status.chair_id
+ WHERE
+ -- 最新のライドが完了しているか1度もライドに割り当てられていなくて現在アクティブな椅子を取得する
+ 	(chair_latest_status.ride_status = 'COMPLETED' OR chair_latest_status.ride_status IS NULL) AND chairs.is_active`,
+		lat, lon, distance,
 	)
 	if err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	nearbyChairs := make([]appGetNearbyChairsResponseChair, 0, len(chairsWithDetails))
-	for _, chair := range chairsWithDetails {
+	nearbyChairs := make([]appGetNearbyChairsResponseChair, 0, len(chairs))
+	for _, chair := range chairs {
 		nearbyChairs = append(nearbyChairs, appGetNearbyChairsResponseChair{
-			ID:    chair.ChairID,
-			Name:  chair.ChairName,
-			Model: chair.ChairModel,
+			ID:    chair.ID,
+			Name:  chair.Name,
+			Model: chair.Model,
 			CurrentCoordinate: Coordinate{
 				Latitude:  chair.Latitude,
 				Longitude: chair.Longitude,
